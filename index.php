618edefcf4a6c8ce54cf5ca0fe9e44db078cd8e9
jponce <albertojoseponce@gmail.com>
2017-05-28 12:40:37 -0300

    holaaa

--- index.php
+++ index.php
@@ -148,28 +148,6 @@ ABOUT SECTION START
 
 <hr><!-- SECTION SEPARETOR LINE -->
 <?php include 'download.php'; ?>
-<!-- ===========================
-PORTFOLIO SECTION START
-=========================== -->
-<!--<div id="portfolio">-->
-<!--    <div class="sectionhead wow bounceInUp" data-wow-duration="2s">-->
-<!--        <span class="bigicon icon-rocket"></span>-->
-<!--        <h3>A few recent works</h3>-->
-<!--        <hr class="separetor">-->
-<!--    </div><!-- PORTFOLIO SECTION HEAD END -->-->
-<!---->
-<!--    <!-- PORTFOLIO ITEMS START -->-->
-<!--    <div class="portfolioitems container">-->
-<!--        <ul><div id="shotsByPlayerId"> </div></ul><!--REFER TO THE js/drifolio.js-->-->
-<!--    </div><!-- PORTFOLIO ITEMS END -->-->
-<!---->
-<!--    <!--Replace the URL below with your dribbble profile URL-->-->
-<!--    <a class="btn btn-default wow fadeInUp" href="http://dribbble.com/srizon" role="button">-->
-<!--        <span class="btnicon icon-social-dribbble"></span>-->
-<!--        <span class="button_text">View all items</span>-->
-<!--    </a>-->
-<!--</div><!-- PORTFOLIO SECTION END -->-->
-
 <?php include 'comentarios.php'; ?>
 <?php include 'footer.php'; ?>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
